ed619ba20fe49090fa4cf3b8622d2a0401416f94
moonrailgun <moonrailgun@gmail.com>
2020-07-07 18:07:30 +0800

    add custom theme

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,8 +1,14 @@
 import React from 'react';
-
 import { NavBar } from './components/NavBar';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import { MainRoute } from './routes/MainRoute';
+import { ThemeProvider } from '@material-ui/core';
+import { theme } from './theme';
+
+const Provider: React.FC = React.memo((props) => {
+  return <ThemeProvider theme={theme}>{props.children}</ThemeProvider>;
+});
+Provider.displayName = 'Provider';
 
 export const App: React.FC = React.memo(() => {
   return (
---
+++ client/src/theme.ts
@@ -0,0 +1,11 @@
+import { createMuiTheme } from '@material-ui/core/styles';
+
+export const theme = createMuiTheme({
+  palette: {
+    type: 'light',
+    primary: {
+      main: '#da8501',
+      contrastText: '#ffffff',
+    },
+  },
+});
